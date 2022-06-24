c5af49fe6f551327a02eb6e6fb824fb085b8024c
Clément Gaudet <derkeethus96@gmail.com>
2022-06-24 23:25:03 +0200

    removed dumb joke

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 
-@bot.slash_command(guild_ids=[587659892425228348], name = "shame", description = "Shame Danneth")
-async def shame(ctx):
-    user = ctx.interaction.user
-    await ctx.respond(f'<@185368034087665664> likes to succ {utils.user_to_ping(user)} a lot!')
-
-
 @bot.slash_command(guild_ids=[587659892425228348], name = "searchcharacter", description = "Search for an FFXIV character")
 async def searchcharacter(ctx, world: discord.Option(str), forename: discord.Option(str), surname: discord.Option(str)):
     character = await ffxivapi.search_character(world, forename, surname)
