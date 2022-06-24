c70633f6c7b5c355dbd83c4582444080e4d77b56
Clément Gaudet <derkeethus96@gmail.com>
2022-06-24 23:24:48 +0200

    expanded searchcharacter to show more info

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os
 import utils
+import ffxivapi
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,9 +12,44 @@ bot = discord.Bot(debug_guilds=[587659892425228348])
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+
 @bot.slash_command(guild_ids=[587659892425228348], name = "shame", description = "Shame Danneth")
 async def shame(ctx):
     user = ctx.interaction.user
     await ctx.respond(f'<@185368034087665664> likes to succ {utils.user_to_ping(user)} a lot!')
 
-bot.run(os.getenv('TOKEN'))
+
+@bot.slash_command(guild_ids=[587659892425228348], name = "searchcharacter", description = "Search for an FFXIV character")
+async def searchcharacter(ctx, world: discord.Option(str), forename: discord.Option(str), surname: discord.Option(str)):
+    character = await ffxivapi.search_character(world, forename, surname)
+    if character is None:
+        await ctx.respond(f'Could not find {forename} {surname} on server {world}')
+        return
+
+    character_info = await ffxivapi.get_character(character['ID'])
+    if character_info is None:
+        await ctx.respond(f'Could not find {forename} {surname} on server {world}')
+        return
+
+    embed= discord.Embed(
+        title=character['Name'],
+        color=discord.Colour.blurple()
+    )
+    embed.set_thumbnail(url=character['Avatar'])
+
+    embed.add_field(name='Server', value=character['Server'], inline=False)
+    embed.add_field(name='Main Job', value=character_info['Character']['ActiveClassJob']['UnlockedState']['Name'], inline=False)
+
+    jobs = character_info['Character']['ClassJobs']
+    for job in jobs[0:20]:
+        level = int(job['Level'])
+        if level == 90:
+            textlevel = f'***{level}***'
+        else:
+            textlevel = str(level)
+        embed.add_field(name=job['UnlockedState']['Name'], value=textlevel, inline=True)
+
+    await ctx.respond(embed=embed)
+
+
+bot.run(os.getenv('BOT_TOKEN'))
